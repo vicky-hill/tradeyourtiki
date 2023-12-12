4acb49a370b9e7b5e135b8e2966fc333780f1045
vicky-hill <vicky.ungemach95@gmail.com>
2023-12-12 05:39:05 -0800

    create product grid for homepage

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,28 @@
-import { dabrush } from "./layout";
+import { dabrush } from './layout'
+import api from '@/utils/api'
+import Container from '@/components/layout/Container';
 
 const Home = async ({ }) => {
 
+    const products = await api.get('products');
+    
+
     return (
-        <div className='mt-5 text-center'>
+        <Container className='text-center'>
             <h1 className={`text-6xl ${dabrush.className}`}>Trade your Tiki</h1>
-        </div>
+
+            <div className="mt-20 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-4">
+                {
+                    products.map(product => (
+                        <div>
+                            <h2>{product.name}</h2>
+                            <p>{product.shortDesc}</p>
+                            <p>${product.price}</p>
+                        </div>
+                    ))
+                }
+            </div>
+        </Container>
     )
 }
 
