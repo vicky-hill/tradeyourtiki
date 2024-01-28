a8a3512b00e8cdc24be4101ed88d65ad08a682d9
vicky-hill <vicky.ungemach95@gmail.com>
2024-01-28 06:42:22 -0800

    create page for gap, space and size

---
+++ src/app/tailwind/gap/page.js
@@ -0,0 +1,114 @@
+'use client'
+
+export default function page({ }) {
+
+    return (
+        <div className='flex code ml-8 my-12 space-x-20'>
+            <div className='my-12 space-y-7 code'>
+                <div className='bg-sky-200 rounded px-4 py-2 w-max'>
+                    space-x
+                </div>
+                <div className='space-y-5'>
+                    <p className='ml-2'>space-x-0</p>
+                    <p className='ml-2'>space-x-px</p>
+                    <p className='ml-2'>space-x-1</p>
+                    <p className='ml-2'>space-x-1.5</p>
+                    <p className='ml-2'>space-x-2</p>
+                    <p className='ml-2'>...</p>
+                    <p className='ml-2'>space-x-96</p>
+                    <p className='ml-2'>space-x-reverse</p>
+                    
+                </div>
+            </div>
+
+            <div className='my-12 space-y-7 code'>
+                <div className='bg-sky-200 rounded px-4 py-2 w-max'>
+                    space-y
+                </div>
+                <div className='space-y-5'>
+                    <p className='ml-2'>space-y-0</p>
+                    <p className='ml-2'>space-y-px</p>
+                    <p className='ml-2'>space-y-1</p>
+                    <p className='ml-2'>space-y-1.5</p>
+                    <p className='ml-2'>space-y-2</p>
+                    <p className='ml-2'>...</p>
+                    <p className='ml-2'>space-y-96</p>
+                    <p className='ml-2'>space-y-reverse</p>
+                </div>
+            </div>
+
+            <div className='my-12 space-y-7 code'>
+                <div className='bg-sky-200 rounded px-4 py-2 w-max'>
+                    gap
+                </div>
+                <div className='space-y-5'>
+                    <p className='ml-2'>gap-0</p>
+                    <p className='ml-2'>gap-px</p>
+                    <p className='ml-2'>gap-1</p>
+                    <p className='ml-2'>gap-1.5</p>
+                    <p className='ml-2'>gap-2</p>
+                    <p className='ml-2'>...</p>
+                    <p className='ml-2'>gap-96</p>
+                </div>
+            </div>
+
+            <div className='my-12 space-y-7 code'>
+                <div className='bg-sky-200 rounded px-4 py-2 w-max'>
+                    gap-x
+                </div>
+                <div className='space-y-5'>
+                    <p className='ml-2'>gap-x-0</p>
+                    <p className='ml-2'>gap-x-px</p>
+                    <p className='ml-2'>gap-x-1</p>
+                    <p className='ml-2'>gap-x-1.5</p>
+                    <p className='ml-2'>gap-x-2</p>
+                    <p className='ml-2'>...</p>
+                    <p className='ml-2'>gap-x-96</p>
+                </div>
+            </div>
+
+            <div className='my-12 space-y-7 code'>
+                <div className='bg-sky-200 rounded px-4 py-2 w-max'>
+                    gap-y
+                </div>
+                <div className='space-y-5'>
+                    <p className='ml-2'>gap-y-0</p>
+                    <p className='ml-2'>gap-y-px</p>
+                    <p className='ml-2'>gap-y-1</p>
+                    <p className='ml-2'>gap-y-1.5</p>
+                    <p className='ml-2'>gap-y-2</p>
+                    <p className='ml-2'>...</p>
+                    <p className='ml-2'>gap-y-96</p>
+                </div>
+            </div>
+
+            <div className='my-12 space-y-7 code'>
+                <div className='bg-sky-200 rounded px-4 py-2 w-max'>
+                    size
+                </div>
+                <div className='space-y-5'>
+                    <p className='ml-2'>size-0</p>
+                    <p className='ml-2'>size-px</p>
+                    <p className='ml-2'>size-1</p>
+                    <p className='ml-2'>size-1.5</p>
+                    <p className='ml-2'>size-2</p>
+                    <p className='ml-2'>...</p>
+                    <p className='ml-2'>size-96</p>
+                </div>
+            </div>
+
+            <div className='my-12 space-y-7 code'>
+                <div className='bg-sky-200 rounded px-4 py-2 w-max'>
+                    size
+                </div>
+                <div className='space-y-5'>
+                    <p className='ml-2'>size-auto</p>
+                    <p className='ml-2'>size-full</p>
+                    <p className='ml-2'>size-min</p>
+                    <p className='ml-2'>size-max</p>
+                    <p className='ml-2'>size-fit</p>
+                </div>
+            </div>
+        </div>
+    )
+}
