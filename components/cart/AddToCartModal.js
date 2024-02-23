cecd136801bfd3d12e828392f97ea58408194523
vicky-hill <vicky.ungemach95@gmail.com>
2024-02-23 05:18:51 -0800

    fix add to cart image issue

--- components/cart/AddToCartModal.js
+++ components/cart/AddToCartModal.js
@@ -1,3 +1,4 @@
+import { useState, useEffect } from 'react'
 import Button from '@/components/elements/Button'
 import Modal from '@/components/elements/Modal'
 import Image from '@/next/Image'
@@ -7,22 +8,35 @@ import Link from 'next/link'
 
 
 const AddToCartModal = ({ modal, close, product }) => {
+    const [details, setDetails] = useState(null);
+
+    useEffect(() => {
+        setDetails(product)
+    }, [product])
+
+    const onClose = () => {
+        setTimeout(() => {
+            setDetails(null);
+        }, 500);
+    
+        close();
+    }
+
 
     return (
         <Modal title="Modal" open={modal} close={close} className="add-to-cart-modal" shouldCloseOnOutsideClick>
             <h3>Item was added to your cart</h3>
             {
-                product && (
+                details && (
                     <div className="add-to-cart-modal__product">
                         <div className="add-to-cart-modal__product-image">
                             <Image
-                                src={product.image}
+                                src={details.image}
                                 className="add-to-cart-modal__product-image--inner"
-                                sizes="4rem"
                             />
                         </div>
                         <div className="add-to-cart-modal__product-info">
-                            <p className='add-to-cart-modal__product-name'>{product.name}</p>
+                            <p className='add-to-cart-modal__product-name'>{details.name}</p>
                             <p className='add-to-cart-modal__product-price'>{price(product.price)}</p>
                         </div>
                     </div>
@@ -33,7 +47,7 @@ const AddToCartModal = ({ modal, close, product }) => {
                     <Button onClick={close} className="me-2 add-to-cart-modal__btn">View Cart</Button>
                 </Link>
 
-                <Button className='add-to-cart-modal__btn' onClick={close} outline>Continue Shopping</Button>
+                <Button className='add-to-cart-modal__btn' onClick={onClose} outline>Continue Shopping</Button>
             </div>
         </Modal>
     )
